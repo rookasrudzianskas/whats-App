1b0e6ecdee88905a546782dde11e975c4cd865f3
Rokas <rookas.rudzenskas@gmail.com>
2022-10-27 13:29:22 +0300

    Rokas

--- src/components/ChatListItem/ChatListItem.js
+++ src/components/ChatListItem/ChatListItem.js
@@ -1,21 +1,26 @@
 //@ts-nocheck
 import React from 'react';
 import {Text, View, StyleSheet, TouchableOpacity, Image} from 'react-native';
+import {EvilIcons, MaterialIcons} from "@expo/vector-icons";
 
 const ChatListItem = () => {
     return (
-        <TouchableOpacity activeOpacity={0.7} className="flex-row mt-20 items-center mx-4">
+        <TouchableOpacity activeOpacity={0.7} className="flex-row mt-20 items-start mx-4">
             <View className="">
-                <Image className="w-[55px]  h-[55px] rounded-full" source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg'}} />
+                <Image className="w-[55px] h-[55px] rounded-full" source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg'}} />
             </View>
             <View className="flex-1 justify-center ml-3">
                 <View className="flex-row justify-between items-center">
-                    <Text className="text-lg font-semibold">Rokas</Text>
+                    <Text numberOfLines={1} className="text-lg font-semibold">Rokas</Text>
                     <Text className="text-gray-500 text-[15px]">07:30</Text>
                 </View>
-                <Text className="text-gray-500 text-[15px]">
-                    Oke
-                </Text>
+                <View className="space-x-1 flex-row items-center">
+                    {/*<EvilIcons name="check" size={16} color="green" />*/}
+                    <Text numberOfLines={2} className="text-gray-500 text-[15px]">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci at consequatur dolores error ex ipsa maxime numquam officiis possimus quis.
+                    </Text>
+                </View>
+                <View className=" border-b border-gray-300 mt-3"/>
             </View>
         </TouchableOpacity>
     );
