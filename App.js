4198f2bdfefd719240060b6921f7c45fe27127e0
Rokas <rookas.rudzenskas@gmail.com>
2022-10-27 20:33:49 +0300

    Rokas

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import Navigation from "./src/navigation";
-import {Amplify, Auth} from 'aws-amplify'
+import {Amplify, API, Auth, graphqlOperation} from 'aws-amplify'
 import awsconfig from './src/aws-exports';
 import {withAuthenticator} from "aws-amplify-react-native/src/Auth";
 import {useEffect, useState} from "react";
 import LoadingIndicator from "./src/components/LoadingIndicator";
+import {getUser} from "./src/graphql/queries";
 
 Amplify.configure({
     ...awsconfig,
@@ -21,7 +22,17 @@ const App = () => {
             // get Auth user
             const authUser = await Auth.currentAuthenticatedUser({ bypassCache: true });
             // query the database using the Auth user ID
+            const userData = await API.graphql(graphqlOperation(getUser, { id: authUser.attributes.sub }));
             // if there is no users in db, create one
+            if(userData.data.getUser) {
+                console.log("User is already registered in database");
+                setLoading(false);
+                return;
+            } else {
+                // create a new user
+
+            }
+
         })();
     }, []);
 
