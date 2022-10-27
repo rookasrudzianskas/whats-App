3c01fbdfc4bf5297d6c6a161acf8ef1eb8b2ab4c
Rokas <rookas.rudzenskas@gmail.com>
2022-10-27 14:19:35 +0300

    Rokas

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,13 +1,28 @@
 //@ts-nocheck
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import dayjs from 'dayjs';
+dayjs.extend(relativeTime);
+
 
 const Message = ({message}) => {
     return (
-        <View>
+        <View style={styles.container}>
             <Text>{message?.text}</Text>
+            <Text style={styles.time}>{dayjs(message?.createdAt).fromNow() || 'Loading...'}</Text>
         </View>
     );
 };
 
 export default Message;
+
+const styles = StyleSheet.create({
+    container: {
+
+    },
+    time: {
+        color: 'grey',
+        alignSelf: 'flex-end',
+    }
+});
