0b53f2dc308ea1dad1178e7e11b11ed02b25030d
Rokas <rookas.rudzenskas@gmail.com>
2022-10-27 13:15:39 +0300

    UI #1

--- src/components/ChatListItem/ChatListItem.js
+++ src/components/ChatListItem/ChatListItem.js
@@ -1,14 +1,23 @@
 //@ts-nocheck
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, TouchableOpacity, Image} from 'react-native';
 
 const ChatListItem = () => {
     return (
-        <View>
-            <Text>
-                byrookas 🚀
-            </Text>
-        </View>
+        <TouchableOpacity activeOpacity={0.7} className="flex-row mt-20 items-center mx-4">
+            <View className="">
+                <Image className="w-[55px]  h-[55px] rounded-full" source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg'}} />
+            </View>
+            <View className="flex-1 justify-center ml-3">
+                <View className="flex-row justify-between items-center">
+                    <Text className="text-lg font-semibold">Rokas</Text>
+                    <Text className="text-gray-500 text-[15px]">07:30</Text>
+                </View>
+                <Text className="text-gray-500 text-[15px]">
+                    Oke
+                </Text>
+            </View>
+        </TouchableOpacity>
     );
 };
 
