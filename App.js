f557150967dc888ccd19baf3708a9560b2d89a6b
Rokas <rookas.rudzenskas@gmail.com>
2022-10-27 17:21:24 +0300

    Rokas

--- App.js
+++ App.js
@@ -1,10 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import ChatScreen from "./src/screens/ChatScreen";
-import {NavigationContainer} from "@react-navigation/native";
 import Navigation from "./src/navigation";
 import { Amplify } from 'aws-amplify'
 import awsconfig from './src/aws-exports';
-// import withAuthenticator
 import {withAuthenticator} from "aws-amplify-react-native/src/Auth";
 
 Amplify.configure({
