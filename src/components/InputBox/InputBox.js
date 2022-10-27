7ee6a89f1e322d0c773cd25d61bbc7ec44502565
Rokas <rookas.rudzenskas@gmail.com>
2022-10-27 14:49:03 +0300

    Rokas

--- src/components/InputBox/InputBox.js
+++ src/components/InputBox/InputBox.js
@@ -5,17 +5,17 @@ import {AntDesign, Ionicons} from "@expo/vector-icons";
 
 const InputBox = () => {
     return (
-        <View className="flex-row items-center bg-white px-3 py-2 mb-5">
+        <View className="flex-row items-center bg-white px-3 py-2 pb-7">
             <TouchableOpacity activeOpacity={0.7}>
-                <AntDesign name="plus" size={24} color="#34B7F1" />
+                <AntDesign name="plus" size={24} color="#0090ff" />
             </TouchableOpacity>
             <View className="flex-1 flex-row items-center border border-gray-300 rounded-full mx-3 px-3 py-[5px]">
                 <TextInput placeholder={'Type your message here.'} className="flex-1" />
                 <TouchableOpacity activeOpacity={0.7} className="">
-                    <Ionicons name="document-attach" size={19} color="#34B7F1" />
+                    <Ionicons name="document-attach" size={19} color="#0090ff" />
                 </TouchableOpacity>
             </View>
-            <TouchableOpacity activeOpacity={0.7} className="bg-[#34B7F1] w-9 items-center justify-center rounded-full overflow-hidden h-9">
+            <TouchableOpacity activeOpacity={0.7} className="bg-[#0090ff] w-9 items-center justify-center rounded-full overflow-hidden h-9">
                 <Ionicons name="ios-send-sharp" size={20} style={{marginLeft: 3}} color="white" />
             </TouchableOpacity>
         </View>
--- src/screens/ChatScreen/ChatScreen.js
+++ src/screens/ChatScreen/ChatScreen.js
@@ -1,6 +1,6 @@
 //@ts-nocheck
 import React from 'react';
-import {Text, View, StyleSheet, ImageBackground, FlatList} from 'react-native';
+import {Text, View, StyleSheet, ImageBackground, FlatList, KeyboardAvoidingView, Platform} from 'react-native';
 import bg from '../../../assets/images/BG.png';
 import messagesData from '../../../assets/data/messages.json';
 import Message from "../../components/Message";
@@ -8,23 +8,31 @@ import InputBox from "../../components/InputBox";
 
 const ChatScreen = () => {
     return (
-        <ImageBackground source={bg} className="h-full">
-            <FlatList
-                data={messagesData}
-                showsVerticalScrollIndicator={false}
-                style={styles.list}
-                inverted={true}
-                renderItem={({item}) => (
-                <Message message={item} />
-            )} />
-            <InputBox />
-        </ImageBackground>
+        <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            style={styles.bg}
+        >
+            <ImageBackground source={bg} className="h-full">
+                <FlatList
+                    data={messagesData}
+                    showsVerticalScrollIndicator={false}
+                    style={styles.list}
+                    inverted={true}
+                    renderItem={({item}) => (
+                    <Message message={item} />
+                )} />
+                <InputBox />
+            </ImageBackground>
+        </KeyboardAvoidingView>
     );
 };
 
 export default ChatScreen;
 
 const styles = StyleSheet.create({
+    bg: {
+
+    },
    list: {
        padding: 10,
    }
