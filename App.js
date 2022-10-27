17f90048471279bb2d24d6dab8ece89cce385461
Rokas <rookas.rudzenskas@gmail.com>
2022-10-27 14:07:39 +0300

    Rokas

--- App.js
+++ App.js
@@ -3,11 +3,12 @@ import {FlatList, StyleSheet, Text, View} from 'react-native';
 import ChatListItem from "./src/components/ChatListItem";
 import chatsData from './assets/data/chats.json';
 import ChatsScreen from "./src/screens/ChatsScreen";
+import ChatScreen from "./src/screens/ChatScreen";
 
 const App = () => {
     return (
         <View className="py-16">
-            <ChatsScreen />
+            <ChatScreen />
             <StatusBar style="auto" />
         </View>
     );
