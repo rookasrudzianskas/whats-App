afaa978f20d1b8342efa5a3c518034ea6d348671
Rokas <rookas.rudzenskas@gmail.com>
2022-10-28 11:34:53 +0300

    Rokas

--- src/services/chatRoomService.js
+++ src/services/chatRoomService.js
@@ -7,14 +7,14 @@ export const getCommonChatRoomWithUser = async (userID) => {
     const response = await API.graphql(graphqlOperation(listChatRooms, { id: authUser.attributes.sub }));
     const chatRooms = response.data?.getUser?.ChatRooms?.items || [];
 
-    console.log(chatRooms[0].chatRoom.users.items[0]);
+    // console.log(chatRooms[0].chatRoom.users.items[0]);
 
     const chatRoom = chatRooms.find((chatRoomItem) => {
         return chatRoomItem.chatRoom.users.items.some(
             (userItem) => userItem.user.id === userID
         );
     });
-    console.log(chatRoom);
+    // console.log(chatRoom);
 
     return chatRoom;
     // console.log(response);
