dbd5a12e7c6d8bd6dcfda731fd73e470f827d1c0
Rokas <rookas.rudzenskas@gmail.com>
2022-10-28 09:13:48 +0300

    Rokas

--- src/screens/ChatsScreen/ChatsScreen.js
+++ src/screens/ChatsScreen/ChatsScreen.js
@@ -3,7 +3,8 @@ import React, {useEffect} from 'react';
 import {Text, View, StyleSheet, FlatList} from 'react-native';
 import chatsData from "../../../assets/data/chats.json";
 import ChatListItem from "../../components/ChatListItem";
-import {Auth} from "aws-amplify";
+import {API, Auth, graphqlOperation} from "aws-amplify";
+import {listChatRooms} from "./queries";
 
 const ChatsScreen = () => {
 
@@ -12,6 +13,8 @@ const ChatsScreen = () => {
             // fetch chats
             const authUser = await Auth.currentAuthenticatedUser({ bypassCache: true });
             // console.log(authUser.attributes.sub);
+            const response = await API.graphql(graphqlOperation(listChatRooms, { id: authUser.attributes.sub }));
+            console.log(response);
         }
         fetchChats();
     }, []);
