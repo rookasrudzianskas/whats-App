71b1347c1fb433dcbc8d4102082d99456053d2e5
Rokas <rookas.rudzenskas@gmail.com>
2022-10-29 16:11:22 +0300

    Rokas

--- src/screens/ContactsScreen/ContactsScreen.js
+++ src/screens/ContactsScreen/ContactsScreen.js
@@ -1,9 +1,10 @@
-import {ActivityIndicator, FlatList, View} from 'react-native';
+import {ActivityIndicator, FlatList, Pressable, Text, TouchableOpacity, View} from 'react-native';
 import ContactListItem from '../../components/ContactListItem';
 import {useEffect, useState} from "react";
 import {API, graphqlOperation} from "aws-amplify";
 import {listUsers} from "../../graphql/queries";
 import LoadingIndicator from "../../components/LoadingIndicator";
+import {MaterialIcons} from "@expo/vector-icons";
 
 const ContactsScreen = () => {
     const [users, setUsers] = useState([]);
@@ -24,6 +25,29 @@ const ContactsScreen = () => {
                 data={users}
                 renderItem={({ item }) => <ContactListItem user={item} />}
                 style={{ backgroundColor: 'white' }}
+                ListHeaderComponent={() => (
+                    <TouchableOpacity className="border rounded border-gray-300 mx-3 py-2 px-3 mb-4" activeOpacity={0.7}
+                        onPress={() => {}}
+                        style={{
+                            flexDirection: "row",
+                            alignItems: "center",
+                        }}
+                    >
+                        <MaterialIcons
+                            name="group"
+                            size={24}
+                            color="royalblue"
+                            style={{
+                                marginRight: 20,
+                                backgroundColor: "gainsboro",
+                                padding: 7,
+                                borderRadius: 20,
+                                overflow: "hidden",
+                            }}
+                        />
+                        <Text className="font-semibold text-blue-500" style={{ fontSize: 16 }}>New Group</Text>
+                    </TouchableOpacity>
+                )}
             />
         </View>
     );
