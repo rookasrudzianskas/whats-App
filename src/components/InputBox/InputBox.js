70935c5a4fa8f62e0af1a496ab92eee55ea542f0
Rokas <rookas.rudzenskas@gmail.com>
2022-11-04 18:30:05 +0200

    Rokas

--- src/components/InputBox/InputBox.js
+++ src/components/InputBox/InputBox.js
@@ -1,7 +1,7 @@
 //@ts-nocheck
 import React, {useState} from 'react';
-import {Text, View, StyleSheet, TouchableOpacity, TextInput, SafeAreaView} from 'react-native';
-import {AntDesign, Ionicons} from "@expo/vector-icons";
+import {Text, View, StyleSheet, TouchableOpacity, TextInput, SafeAreaView, Image} from 'react-native';
+import {AntDesign, Ionicons, MaterialIcons} from "@expo/vector-icons";
 import {useSafeAreaInsets} from "react-native-safe-area-context";
 import {API, Auth, graphqlOperation} from "aws-amplify";
 import {createMessage, updateChatRoom} from "../../graphql/mutations";
@@ -55,22 +55,41 @@ const InputBox = ({chatRoom}) => {
 
 
     return (
-        <SafeAreaView edges={['bottom']} style={styles.container}>
-           <View className="flex-row items-center px-3 bg-white">
-               <TouchableOpacity onPress={pickImage} activeOpacity={0.7} className="py-3">
-                   <AntDesign name="plus" size={24} color="#0090ff" />
-               </TouchableOpacity>
-               <View className="flex-1 flex-row items-center border border-gray-300 rounded-full mx-3 px-3 py-[5px]">
-                   <TextInput value={text} onChangeText={(text) => setText(text)} placeholder={'Type your message here.'} className="flex-1" />
-                   <TouchableOpacity activeOpacity={0.7} className="">
-                       <Ionicons name="document-attach" size={19} color="#0090ff" />
-                   </TouchableOpacity>
-               </View>
-               <TouchableOpacity disabled={loading} onPress={() => onSend()} activeOpacity={0.7} className={`${loading ? 'bg-gray-300' : 'bg-[#0090ff]'} w-9 items-center justify-center rounded-full overflow-hidden h-9`}>
-                   <Ionicons name="ios-send-sharp" size={20} style={{marginLeft: 3}} color="white" />
-               </TouchableOpacity>
-           </View>
-        </SafeAreaView>
+        <>
+            {image && (
+                <View style={styles.attachmentsContainer}>
+                    <Image
+                        source={{ uri: image }}
+                        style={styles.selectedImage}
+                        resizeMode="contain"
+                    />
+                    <TouchableOpacity style={styles.removeSelectedImage} activeOpacity={0.7}>
+                        <MaterialIcons
+                            name="highlight-remove"
+                            onPress={() => setImage(null)}
+                            size={20}
+                            color="gray"
+                        />
+                    </TouchableOpacity>
+                </View>
+            )}
+            <SafeAreaView edges={['bottom']} style={styles.container}>
+                <View className="flex-row items-center px-3 bg-white">
+                    <TouchableOpacity onPress={pickImage} activeOpacity={0.7} className="py-3">
+                        <AntDesign name="plus" size={24} color="#0090ff" />
+                    </TouchableOpacity>
+                    <View className="flex-1 flex-row items-center border border-gray-300 rounded-full mx-3 px-3 py-[5px]">
+                        <TextInput value={text} onChangeText={(text) => setText(text)} placeholder={'Type your message here.'} className="flex-1" />
+                        <TouchableOpacity activeOpacity={0.7} className="">
+                            <Ionicons name="document-attach" size={19} color="#0090ff" />
+                        </TouchableOpacity>
+                    </View>
+                    <TouchableOpacity disabled={loading} onPress={() => onSend()} activeOpacity={0.7} className={`${loading ? 'bg-gray-300' : 'bg-[#0090ff]'} w-9 items-center justify-center rounded-full overflow-hidden h-9`}>
+                        <Ionicons name="ios-send-sharp" size={20} style={{marginLeft: 3}} color="white" />
+                    </TouchableOpacity>
+                </View>
+            </SafeAreaView>
+        </>
     );
 };
 
@@ -83,5 +102,20 @@ const styles = StyleSheet.create({
         padding: 5,
         paddingHorizontal: 10,
         alignItems: 'center',
+    },
+    attachmentsContainer: {
+        alignItems: "flex-end",
+    },
+    selectedImage: {
+        height: 100,
+        width: 200,
+        margin: 5,
+    },
+    removeSelectedImage: {
+        position: "absolute",
+        right: 10,
+        backgroundColor: "white",
+        borderRadius: 10,
+        overflow: "hidden",
     },
 });
