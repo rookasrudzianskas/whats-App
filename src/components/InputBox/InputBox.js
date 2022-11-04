0996f33c61152f545724abb545c75e5a910c456a
Rokas <rookas.rudzenskas@gmail.com>
2022-11-04 19:17:14 +0200

    Rokas

--- src/components/InputBox/InputBox.js
+++ src/components/InputBox/InputBox.js
@@ -6,6 +6,8 @@ import {useSafeAreaInsets} from "react-native-safe-area-context";
 import {API, Auth, graphqlOperation} from "aws-amplify";
 import {createMessage, updateChatRoom} from "../../graphql/mutations";
 import * as ImagePicker from "expo-image-picker";
+import 'react-native-get-random-values';
+import { v4 as uuidv4 } from 'uuid';
 
 const InputBox = ({chatRoom}) => {
     const [text, setText] = useState("");
@@ -50,6 +52,20 @@ const InputBox = ({chatRoom}) => {
 
         if (!result.cancelled) {
             setImage(result.uri);
+        }
+    };
+
+    const uploadFile = async (fileUri) => {
+        try {
+            const response = await fetch(fileUri);
+            const blob = await response.blob();
+            const key = `${uuidv4()}.png`;
+            await Storage.put(key, blob, {
+                contentType: "image/png", // contentType is optional
+            });
+            return key;
+        } catch (err) {
+            console.log("Error uploading file:", err);
         }
     };
 
