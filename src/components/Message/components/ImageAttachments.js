6fff28d87f9efcfbe84119d98a0e76ebeeb799f3
Rokas <rookas.rudzenskas@gmail.com>
2022-11-05 11:09:21 +0200

    Rokas

---
+++ src/components/Message/components/ImageAttachments.js
@@ -0,0 +1,49 @@
+import {Pressable, Image, StyleSheet, TouchableOpacity} from "react-native";
+import { useState } from "react";
+import ImageView from "react-native-image-viewing";
+
+const ImageAttachments = ({ attachments }) => {
+    const [imageViewerVisible, setImageViewerVisible] = useState(false);
+
+    return (
+        <>
+            {attachments.map((attachment) => (
+                <TouchableOpacity
+                    style={[
+                        styles.imageContainer,
+                        downloadAttachments.length === 1 && { flex: 1 },
+                    ]}
+                    key={attachment.uri} activeOpacity={0.7} onPress={() => setImageViewerVisible(true)}>
+                    <Image source={{ uri: attachment.uri }} style={styles.image} />
+                </TouchableOpacity>
+            ))}
+
+            <ImageView
+                images={attachments.map(({ uri }) => ({ uri }))}
+                imageIndex={0}
+                visible={imageViewerVisible}
+                onRequestClose={() => setImageViewerVisible(false)}
+            />
+        </>
+    );
+};
+
+const styles = StyleSheet.create({
+    images: {
+        flexDirection: "row",
+        flexWrap: "wrap",
+    },
+    imageContainer: {
+        width: "50%",
+        aspectRatio: 1,
+        padding: 3,
+    },
+    image: {
+        flex: 1,
+        borderColor: "white",
+        borderWidth: 1,
+        borderRadius: 5,
+    },
+});
+
+export default ImageAttachments;
